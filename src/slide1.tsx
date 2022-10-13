aa681569107fd2a4fb824c70296c5627dcba3668
kim-limit <rlawogks2468@naver.com>
2022-10-13 15:10:24 +0900

    feat : 'add animation'

--- src/slide1.tsx
+++ src/slide1.tsx
@@ -1,3 +1,5 @@
+import { Grid } from "@mui/material";
+import { Box } from "@mui/system";
 import { useEffect, useRef } from "react";
 import { Fade, Zoom } from "react-awesome-reveal";
 import { useNavigate } from "react-router-dom";
@@ -20,15 +22,44 @@ export const Slide1 = () => {
         height: "100vh",
         padding: "0.5px",
         // width: "100%",
-        backgroundColor: "green",
       }}
     >
-      <Fade>
+      <Grid container>
+        <Grid item xs={5}>
+          <Fade duration={3000}>
+            <h2 style={{ fontSize: "4.2rem" }}>{"프리미엄 부동산 금융투자"}</h2>
+            <h3 style={{ fontSize: "3.2rem" }}>
+              {"자산가와 기관들의 투자가치를 당신에게"}
+            </h3>
+          </Fade>
+        </Grid>
+        <Grid item xs={7} display={"flex"}>
+          <Fade duration={3000} direction={"left"}>
+            <Box
+              sx={{
+                width: 300,
+                height: 500,
+                backgroundColor: "grey",
+              }}
+            />
+          </Fade>
+          <Fade duration={3000} direction={"right"}>
+            <Box
+              sx={{
+                width: 300,
+                height: 500,
+                backgroundColor: "grey",
+              }}
+            />
+          </Fade>
+        </Grid>
+      </Grid>
+      {/* <Fade>
         <div
           style={{ width: "100px", height: "100px", backgroundColor: "blue" }}
         ></div>
       </Fade>
-      <Zoom>줌</Zoom>
+      <Zoom>줌</Zoom> */}
     </div>
   );
 };
